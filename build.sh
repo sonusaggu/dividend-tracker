81948b4a44b598b1debb4cf97951a3875a2a33c2
“Sonusaggu” <sonusaggu92@gmail.com>
2025-08-31 03:23:59 -0600

    build for super user

--- build.sh
+++ build.sh
@@ -8,5 +8,16 @@ pip install -r requirements.txt
 # Collect static files
 python manage.py collectstatic --no-input
 
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser(
+        username='stockfolio_user',
+        email='admin@example.com',
+        password='stockfolio_pass'
+    )
+END
+
 # Apply database migrations
 python manage.py migrate
